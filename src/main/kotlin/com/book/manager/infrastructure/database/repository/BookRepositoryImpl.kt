a2f04e67d546a4859faffb7a6aa66f98adad81eb
masa720 <gtrist_gtdx@yahoo.co.jp>
2023-01-07 15:04:15 +0900

    [add]登録、修正、削除機能の追加

--- src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
+++ src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
@@ -4,15 +4,18 @@ import com.book.manager.domain.model.Book
 import com.book.manager.domain.model.BookWithRental
 import com.book.manager.domain.model.Rental
 import com.book.manager.domain.repository.BookRepository
-import com.book.manager.infrastructure.database.mapper.BookWithRentalMapper
-import com.book.manager.infrastructure.database.mapper.select
-import com.book.manager.infrastructure.database.mapper.selectByPrimaryKey
+import com.book.manager.infrastructure.database.mapper.*
+import com.book.manager.infrastructure.database.record.BookRecord
 import com.book.manager.infrastructure.database.record.BookWithRentalRecord
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
 
 @Suppress("SpringJavaInjectionPointsAutowiringInspection")
 @Repository
-class BookRepositoryImpl(private val bookWithRentalMapper: BookWithRentalMapper) : BookRepository {
+class BookRepositoryImpl(
+  private val bookWithRentalMapper: BookWithRentalMapper,
+  private val bookMapper: BookMapper
+  ) : BookRepository {
   override fun findAllWithRental(): List<BookWithRental> {
     return bookWithRentalMapper.select().map { toModel(it) }
   }
@@ -37,5 +40,21 @@ class BookRepositoryImpl(private val bookWithRentalMapper: BookWithRentalMapper)
       )
     }
     return BookWithRental(book, rental)
+  }
+
+  override fun register(book: Book) {
+    bookMapper.insert(toRecord(book))
+  }
+
+  private fun toRecord(model: Book): BookRecord {
+    return BookRecord(model.id, model.title, model.author, model.releaseDate)
+  }
+
+  override fun update(id: Long, title: String?, author: String?, releaseDate: LocalDate?) {
+    bookMapper.updateByPrimaryKeySelective(BookRecord(id, title, author, releaseDate))
+  }
+
+  override fun delete(id: Long) {
+    bookMapper.deleteByPrimaryKey(id)
   }
 }
